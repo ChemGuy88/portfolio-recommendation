819fd3ab4e74d18558395011d4fdb53e7095aa91
Herman F. Autore <herman@ufl.edu>
2023-11-19 18:34:36 -0500

    Change HTML formatting, add REST API
    Changed HTML template to a more readable format with profile IDs as a header above the profile text.
    
    Added a REST API to the flask web document under the `get_recommendations` method.

--- code/flask-web.py
+++ code/flask-web.py
@@ -1,7 +1,7 @@
 import pickle
 # Third-party packacges
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__, template_folder="flaskTemplates")
 
@@ -20,21 +20,44 @@ data = pd.read_csv("data/output/concatenateData/2023-10-15 19-06-59/Profile Cont
 def template_test():
     if request.method == 'POST':
         selectedLadyID = int(request.form.get('selectedLadyID'))
-        selected_metric = request.form.get('selected_metric')
-        # selectedLadyData = next(ladyID for ladyID in indices if ladyID == selectedLadyID)
+        selectedMetric = request.form.get('selectedMetric')
         selectedLadyData = indices[selectedLadyID]
-        similarLadies = [data["Lady ID"][idx] for idx in selectedLadyData[selected_metric]]
+        similarLadies = [data["Lady ID"][idx] for idx in selectedLadyData[selectedMetric]]
         mask = data["Lady ID"].isin(similarLadies)
         similarProfiles = data["Character"][mask]
+        similarData = zip(similarLadies, similarProfiles)
         return render_template('template.html',
                                list_ladies=list_ladies,
                                selectedLadyID=selectedLadyID,
                                selectedLadyData=selectedLadyData,
                                chosenProfile=data['Character'][data["Lady ID"] == selectedLadyID],
                                similarLadies=similarLadies[:],
-                               similarProfiles=similarProfiles)
+                               similarProfiles=similarProfiles,
+                               similarData=similarData)
     else:
         return render_template('template.html', list_ladies=list_ladies)
+
+
+@app.route("/recommendations", methods=["GET"])
+def get_recommendations():
+    selectedLadyID = request.args.get("ladyID", default=38254, type=int)
+    selectedMetric = request.args.get("metric", default="cosine", type=str)
+    if not selectedLadyID:
+        return jsonify("Missing lady ID"), 400
+    elif selectedMetric not in ["cosine", "euclidean"]:
+        return jsonify("Distance metric can only be cosine or Euclidean"), 400
+    elif selectedLadyID not in list_ladies:
+        return jsonify("This lady ID is not in our database"), 400
+    else:
+        try:
+            selectedLadyData = indices[selectedLadyID]
+            similarLadies = [int(data["Lady ID"][idx]) for idx in selectedLadyData[selectedMetric]]
+            mask = data["Lady ID"].isin(similarLadies)
+            similarProfiles = data["Character"][mask].to_list()
+            similarData = {ladyID: text for ladyID, text in zip(similarLadies, similarProfiles)}
+            return jsonify(similarData), 200
+        except Exception as e:
+            return jsonify(str(e)), 500
 
 
 if __name__ == '__main__':
